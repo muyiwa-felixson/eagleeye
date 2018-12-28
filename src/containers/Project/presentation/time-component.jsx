a41f72f56271e838930486041e9dbc707b9dd10e
Tosin Felixson Yusuf <felixsonyusuftosin@gmail.com>
2018-12-28 02:07:08 +0100

    load videos

--- src/containers/Project/presentation/time-component.jsx
+++ src/containers/Project/presentation/time-component.jsx
@@ -1,35 +1,45 @@
 import React from "react";
 import {
-  TopSection,
-  LowerSection,
-  TimeLine,
   TimeBox,
   TimeDate,
   TimeContent,
   TimeDiv,
   Picture,
-  Video,
-  PayContent,
-  DragZone
+  Video
 } from "../components";
-import {
-  Button,
-  Input,
-  Grid,
-  SimpleSelect,
-  Label,
-  Panel,
-  PaleButton,
-  Aligner,
-  H4,
-  H5,
-  P,
-  ModalComponent,
-  Boxed,
-  TextArea,
-  InputWrapper
-} from "../../../components/flex";
+import { Button, Grid, Label, PaleButton } from "../../../components/flex";
 import { Theme } from "../../../components/flex/theme";
+
+/**
+ * @function fileFilter
+ */
+const videoFilter = extension => {
+  const fileExtension = extension;
+  const acceptedExtensions = [
+    "mp4",
+    "ogx",
+    "3gp",
+    "ogg",
+    "flv",
+    "avi",
+    "quicktime",
+    "mpeg-4",
+    "xdcam",
+    "dnxhd",
+    "vob"
+  ];
+  if (acceptedExtensions.indexOf(fileExtension) < 0) {
+    return false;
+  } else {
+    return true;
+  }
+};
+/**
+ * @function getExtension
+ */
+const getExtension = filename => {
+  return filename.split(".").pop();
+};
 
 export const TimeComponent = ({
   confirmed,
@@ -92,10 +102,26 @@ export const TimeComponent = ({
 
           <div className="media">
             {media.map(elem => {
-              return elem.type === "picture" ? (
+              const isVideo = videoFilter(getExtension(elem.image));
+              return !isVideo ? (
                 <Picture backgroundImage={elem.image} />
               ) : (
-                <Video />
+                <Video>
+                  <video
+                    type="video/mp4"
+                    style={{ width: "100%", height: "100%", display: "block" }}
+                    autoplay={true}
+                    loop={true}
+                    controls={true}
+                    muted={true}
+                  >
+                    <source
+                      // type="video/mp4"
+                      // data-reactid=".0.1.0.0.0"
+                      src={elem.image}
+                    />
+                  </video>
+                </Video>
               );
             })}
           </div>
